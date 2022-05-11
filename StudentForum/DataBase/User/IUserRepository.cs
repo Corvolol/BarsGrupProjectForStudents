03cab3f78cb698acc1891ac3c423df74bd878e9e
Adel <4811102076@mail.ru>
2022-05-11 17:03:45 +0300

    Update UserRepository

--- StudentForum/DataBase/User/IUserRepository.cs
+++ StudentForum/DataBase/User/IUserRepository.cs
@@ -10,5 +10,10 @@ namespace DataBase
     {
         public Task AddUSer(User user);
         public Task<User> GetUser(string email);
+        public Task<bool> UpdateUser (User user);
+        public Task<bool> UpdateUser(string email,Question question);
+        public Task<bool> UpdateUser(string email,Answer answer);
+        public Task<bool> UpdateUser(string email,Review review);
+        public Task<bool> DeleteUser (string email);
     }
 }

fbccf074a75cf7e730b112fac8102ec14f01a40d
adavisly <webayesian@gmail.com>
2022-05-12 12:15:44 +0300

    Models Book and Tickets added

--- StudentForum/DataBase/Context.cs
+++ StudentForum/DataBase/Context.cs
@@ -5,20 +5,26 @@ namespace DataBase
 {
     public class Context: DbContext
     {
-        public  DbSet<User> Users { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
         public DbSet<Answer> Answers { get; set; } = null!;
         public DbSet<Question> Questions { get; set; } = null!;
         public DbSet<Review> Reviews { get; set; } = null!;
         public DbSet<Tag> Tags { get; set; } = null!;
         public DbSet<Teacher> Teachers { get; set; } = null!;
+        public DbSet<Ticket> Tickets { get; set; } = null!;
+        public DbSet<Book> Books { get; set; } = null!;
+
         public Context(DbContextOptions<Context>options) :base(options)
         {
-            Database.EnsureDeleted();
-            Database.EnsureCreated();
+            //Database.EnsureDeleted();
+            //Database.EnsureCreated();
         }
+
+        //dotnet ef migrations add Initial
+        //dotnet ef database update
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite("Data Source=studentforum.db");
+            optionsBuilder.UseSqlServer("Data Source=studentforum.db");
         }
     }
 }

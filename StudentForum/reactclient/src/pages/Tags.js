2950413435f0769cfeaecd05792f1615a85f5319
Corvolol <ledgek.a@gmail.com>
2022-05-16 18:35:33 +0300

    addRouteFront

---
+++ StudentForum/reactclient/src/pages/Tags.js
@@ -0,0 +1,45 @@
+﻿import React, { Component } from 'react';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+
+export default function Tags() {
+    return (
+        <body>
+            <h3>Все теги</h3> <br />
+            <ListGroup flush>
+                <ListGroupItem
+                    action
+                    href="/tag"
+                    tag="a"
+                >
+                    <h4>Тег 1</h4>
+                </ListGroupItem>
+
+                <ListGroupItem
+                    action
+                    href="/tag"
+                    tag="a"
+                >
+                    <h4>Тег 2</h4>
+                </ListGroupItem>
+
+                <ListGroupItem
+                    action
+                    href="/tag"
+                    tag="a"
+                >
+                    <h4>Тег 3</h4>
+                </ListGroupItem>
+
+                <ListGroupItem
+                    action
+                    href="/tag"
+                    tag="a"
+                >
+                    <h4>Тег 4</h4>
+                </ListGroupItem>
+            </ListGroup>
+
+        </body>
+    );
+}
+
